a0cf02f074a5831a50e597aec11452be5fccc822
yangxu351 <xuyang_ustb@163.com>
2022-11-15 03:04:06 -0500

    add 2 classes of WDT

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -2,5 +2,10 @@
 from .coco import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
+# tag: yang added
+from .real_wdt import RealWDT
+from .synthetic_wdt import SyntheticWDT
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
+# __all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
+# tag: yang added
+__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset", "RealWDT", "SyntheticWDT"]
