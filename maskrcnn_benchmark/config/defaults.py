f46c70b7643c87982166215ee9b5e6fe6e8a0cc5
yangxu351 <xuyang_ustb@163.com>
2022-11-15 23:24:18 -0500

    define DATASETS.DATA_SEED=-1

--- maskrcnn_benchmark/config/defaults.py
+++ maskrcnn_benchmark/config/defaults.py
@@ -67,7 +67,7 @@ _C.DATASETS.TARGET_TRAIN = ()
 # List of the dataset names for testing, as present in paths_catalog.py
 _C.DATASETS.TEST = ()
 # tag: yang added
-_C.DATASETS.DATA_SEED = 0
+_C.DATASETS.DATA_SEED = -1
 
 # -----------------------------------------------------------------------------
 # DataLoader
